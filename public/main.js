c2883044e5d88de69ca059cd6f5df3cd7d30f368
Rigney <krigney@proforma.com>
2018-03-26 14:58:34 -0400

    Yay!

--- public/main.js
+++ public/main.js
@@ -1,5 +1,34 @@
-const main = () => {
-  document.querySelector('h1').textContent += '?'
-}
+angular
+  .module("angularScoreboardApp", [])
+  .controller("mainController", ($scope) => {
 
-document.addEventListener('DOMContentLoaded', main)
+    $scope.team1 = "Team 1";
+    $scope.addTeamName1 = () => {
+      $scope.team1 = $scope.team1name;
+    }
+
+    $scope.team2 = "Team 2";
+    $scope.addTeamName2 = () => {
+      $scope.team2 = $scope.team2name;
+    }
+
+    $scope.team1score = 0;
+
+    $scope.subScore1 = () => {
+      $scope.team1score -= 1;
+    }
+
+    $scope.addScore1 = () => {
+      $scope.team1score += 1;
+    }
+
+    $scope.team2score = 0;
+
+    $scope.addScore2 = () => {
+      $scope.team2score += 1;
+    }
+
+    $scope.subScore2 = () => {
+      $scope.team2score += 1;
+    }
+  });
